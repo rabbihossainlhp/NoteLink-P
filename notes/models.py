ae1f704534534a4fea9855ee8c5937b2f9f4ebc5
Rabbi Hossain <rabbihossainlhp@gmail.com>
2025-06-25 19:00:05 +0600

    temporary media file solution 2

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,7 @@ from users.models import CustomUser
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    file_upload = models.FileField(upload_to='media/notes/notes_file', blank=True, null=True)
+    file_upload = models.FileField(upload_to='notes/notes_file', blank=True, null=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
